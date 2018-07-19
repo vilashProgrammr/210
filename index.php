0cfc4ba0b31ad59be2903dfc16448af07ef9972a
rajesh <dj@programmr.com>
2018-07-19 05:36:05 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ include_once 'dbconfig.php';
 if(isset($_GET['delete_id']))
 {
 	//delete logic here
+	//this is dj
+	//used second buffer machine as 1st one is still engaged
+	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+	mysql_query($sql_query);
+	header("Location: $_SERVER[PHP_SELF]");
+	
 	
 }
 // delete condition
